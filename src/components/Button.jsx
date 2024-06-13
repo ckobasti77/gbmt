eb7b79d6f8eed92d13889c22b901f0ab95c28b65
Jovan Milojevic <jovanm028@gmail.com>
2024-06-13 03:50:09 -0700

    changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ const Button = ({ to, text, className }) => {
     <Link
       style={{ transition: "border-radius .3s ease-in-out" }}
       to={to}
-      className={`group opacity-0 translate-y-32 px-6 py-2 -skew-x-[15deg] border-blue-500 border-t-2 border-l-2 rounded-tl-xl rounded-br-xl hover:rounded-tl-none hover:rounded-tr-xl hover:rounded-br-none hover:rounded-bl-xl capitalize backdrop-blur-2xl text-white flex items-center justify-center font-bold ${className}`}
+      className={`text-nowrap group opacity-0 translate-y-32 px-6 py-2 -skew-x-[15deg] border-blue-500 border-t-2 border-l-2 rounded-tl-xl rounded-br-xl hover:rounded-tl-none hover:rounded-tr-xl hover:rounded-br-none hover:rounded-bl-xl capitalize backdrop-blur-2xl text-white flex items-center justify-center font-bold ${className}`}
     >
       <span className="group-hover:skew-x-[15deg] skew-x-[15deg] flex">
         {text}{" "}
